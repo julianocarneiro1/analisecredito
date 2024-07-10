45a3f8d1930784cfc3552a857e2c9046d6f6ef47
Juliano Carneiro <julianoomc@gmail.com>
2024-07-09 21:05:24 -0300

    cria projeto base

---
+++ src/main/java/com/jmc/analisecredito/listener/PropostaEmAnaliseListener.java
@@ -0,0 +1,14 @@
+package com.jmc.analisecredito.listener;
+
+import com.jmc.analisecredito.domain.Proposta;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PropostaEmAnaliseListener {
+
+    @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
+    public void propostaEmAnalise(Proposta proposta) {
+
+    }
+}
